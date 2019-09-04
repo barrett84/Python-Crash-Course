eefccde7882eb718ffc9f1d7f3c3dc2148fc7af2
yinping <barret.yin>
2019-09-04 00:35:21 -0700

    new feature

---
+++ learning_log/users/urls.py
@@ -0,0 +1,8 @@
+"""为应用程序users定义URL模式"""
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+    # 登录页面
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+]
